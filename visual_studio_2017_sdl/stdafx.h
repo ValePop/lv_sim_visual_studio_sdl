e2610114b453ac6cc7c9e6ee3339b2c090d8d032
ValePop <72342951+ValePop@users.noreply.github.com>
2020-11-09 09:55:44 +0100

    Test

--- visual_studio_2017_sdl/stdafx.h
+++ visual_studio_2017_sdl/stdafx.h
@@ -1,10 +1,21 @@
-//#pragma once
+#ifndef STDAFX_H
+#define STDAFX_H
 
-#include "lv_examples/src/lv_ex_get_started/lv_ex_get_started.h"
-#include "lv_examples/src/lv_demo_printer/lv_demo_printer.h"
-#include "lv_examples/src/lv_ex_style/lv_ex_style.h"
-#include "lv_examples/src/lv_ex_widgets/lv_ex_widgets.h"
-#include "lv_examples/src/lv_demo_stress/lv_demo_stress.h"
+#include "lv_examples/lv_examples.h"
+//#include "lv_examples/src/lv_ex_get_started/lv_ex_get_started.h"
+//#include "lv_examples/src/lv_demo_printer/lv_demo_printer.h"
+//#include "lv_examples/src/lv_ex_style/lv_ex_style.h"
+//#include "lv_examples/src/lv_ex_widgets/lv_ex_widgets.h"
+//#include "lv_examples/src/lv_demo_stress/lv_demo_stress.h"
+//#include "lv_examples/src/lv_demo_benchmark/lv_demo_benchmark.h"
+//#include "lv_examples/src/lv_demo_keypad_encoder/lv_demo_keypad_encoder.h"
+//#include "lv_examples/src/lv_demo_printer/lv_demo_printer_theme.h"
+//#include "lv_examples/src/lv_demo_printer/lv_demo_printer.h"
+#include "lvgl/lv_conf_template.h"
+#include "lv_conf.h"
+#include "lv_drv_conf.h"
+#include "lv_ex_conf.h"
+
 
 #include "lvgl/lvgl.h"
 #include <stdlib.h>
@@ -13,4 +24,9 @@
 #include "lv_drivers/display/monitor.h"
 #include "lv_drivers/indev/mouse.h"
 #include "lv_drivers/indev/keyboard.h"
+
+
 //#include ""
+
+
+#endif
